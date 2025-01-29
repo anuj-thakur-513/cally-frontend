0afe4cccf7cc4a25be5fa119fd713242a3763f19
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2025-01-29 21:56:33 +0530

    fix: add null check

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -81,7 +81,7 @@ const Dashboard = () => {
                 </div>
               ))}
             </div>
-          ) : events.length > 0 ? (
+          ) : events?.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {events.map((event) => (
                 <EventCard
